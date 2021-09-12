d2027858aeb93f41b218520ae41a056ee1116ba6
gityash56 <90525149+gityash56@users.noreply.github.com>
2021-09-12 19:51:27 +0530

    Add files via upload
    This is only demo file noting else

---
+++ demo1.java
@@ -0,0 +1,8 @@
+class test
+{
+    public static void main(String[] args)  // command line Argument 
+    {
+        System.out.println("hello coders..!!"); 
+        System.out.println("hello yash..!!");
+    }
+}
